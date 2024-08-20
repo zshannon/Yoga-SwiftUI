92537778096d6dd2cedfa7ab3042aa09d6511c36
Imran Shitta-Bey <pickleboyonline@gmail.com>
2024-08-20 00:15:37 -0500

    monitor flexbox in `sizeThatFits`

--- Sources/YogaSwiftUI/FlexLayoutInspector.swift
+++ Sources/YogaSwiftUI/FlexLayoutInspector.swift
@@ -1,11 +1,14 @@
 public class FlexLayoutInspector {
-  private static var layoutRegistry: [String: LayoutMetrics] = [:]
+    private static var layoutRegistry: [String: LayoutMetrics] = [:]
 
-  public static func getLayoutMetricsFor(flexIntrospectionKey: String) -> LayoutMetrics? {
-    layoutRegistry[flexIntrospectionKey]
-  }
+    /// If enabled, will calculate subviews & update in the ``sizeThatFits`` method
+    public static var enabled: Bool = false
 
-  internal static func setLayoutMetricsFor(flexSubViewID: String, _ layoutMetrics: LayoutMetrics) {
-    layoutRegistry[flexSubViewID] = layoutMetrics
-  }
+    public static func getLayoutMetricsFor(flexIntrospectionKey: String) -> LayoutMetrics? {
+        layoutRegistry[flexIntrospectionKey]
+    }
+
+    static func setLayoutMetricsFor(flexSubViewID: String, _ layoutMetrics: LayoutMetrics) {
+        layoutRegistry[flexSubViewID] = layoutMetrics
+    }
 }
