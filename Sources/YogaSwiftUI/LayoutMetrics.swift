e9e189fb1100d15d544c26f735fc6b8a28d67e1a
Imran Shitta-Bey <pickleboyonline@gmail.com>
2024-08-20 01:18:24 -0500

    Update LayoutMetrics.swift

--- Sources/YogaSwiftUI/LayoutMetrics.swift
+++ Sources/YogaSwiftUI/LayoutMetrics.swift
@@ -1,10 +1,17 @@
-public struct LayoutMetrics {
-  var x: Float
-  var y: Float
-  var width: Float
-  var height: Float
+public struct LayoutMetrics: Equatable {
+    var x: Float
+    var y: Float
+    var width: Float
+    var height: Float
 
-  public static func zero() -> LayoutMetrics {
-    .init(x: 0, y: 0, width: 0, height: 0)
-  }
+    public static func zero() -> LayoutMetrics {
+        .init(x: 0, y: 0, width: 0, height: 0)
+    }
+
+    public init(x: Float, y: Float, width: Float, height: Float) {
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    }
 }
