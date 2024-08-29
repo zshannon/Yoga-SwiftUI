64e49360f0bdd0c6af9cbda63d46296f66a0cab0
Imran Shitta-Bey <pickleboyonline@gmail.com>
2024-08-29 18:28:10 -0400

    make public vars of layout metrics

--- Sources/YogaSwiftUI/LayoutMetrics.swift
+++ Sources/YogaSwiftUI/LayoutMetrics.swift
@@ -1,8 +1,8 @@
 public struct LayoutMetrics: Equatable {
-    var x: Float
-    var y: Float
-    var width: Float
-    var height: Float
+    public var x: Float
+    public var y: Float
+    public var width: Float
+    public var height: Float
 
     public static func zero() -> LayoutMetrics {
         .init(x: 0, y: 0, width: 0, height: 0)
